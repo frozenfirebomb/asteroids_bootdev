9863885c71b7834768799887912a1cc88711b110
frozenfirebomb <bartvrugt@hotmail.com>
2025-12-16 07:27:31 -0500

    Asteroids CH3 L2

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode ((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
 
     while True:
         log_state()
@@ -15,8 +20,9 @@ def main():
 
 
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
-
+        dt = (clock.tick(60) / 1000)
 
 if __name__ == "__main__":
     main()
