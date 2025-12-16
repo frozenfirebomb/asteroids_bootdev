eecd51a06414dfb72f8df6cd0a17cf411a98fc77
frozenfirebomb <bartvrugt@hotmail.com>
2025-12-16 07:48:22 -0500

    ch3 L5:

--- main.py
+++ main.py
@@ -9,7 +9,15 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+
+    
+    
 
     while True:
         log_state()
@@ -20,7 +28,9 @@ def main():
 
 
         screen.fill("black")
-        player.draw(screen)
+        updatable.update(dt)
+        for object in drawable:
+            object.draw(screen)
         pygame.display.flip()
         dt = (clock.tick(60) / 1000)
 
