9814919188804bed709da83a184f5fd1a1bae54a
frozenfirebomb <bartvrugt@hotmail.com>
2025-12-17 07:56:11 -0500

    Asteroids CH4 L6

--- asteroid.py
+++ asteroid.py
@@ -1,6 +1,9 @@
 import pygame
+import random
+
+from logger import log_event
 from circleshape import CircleShape
-from constants import LINE_WIDTH
+from constants import LINE_WIDTH, ASTEROID_MIN_RADIUS
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
@@ -11,3 +14,23 @@ class Asteroid(CircleShape):
 
     def update(self, dt):
         self.position += (self.velocity *dt)
+
+    def split(self):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        else:
+            log_event("asteroid_split")
+
+            angle = random.uniform(20, 50)
+
+            velocity_1 = self.velocity.rotate(angle)
+            velocity_2 = self.velocity.rotate(-angle)
+
+            new_radius = self.radius - ASTEROID_MIN_RADIUS
+
+            asteroid = Asteroid(self.position.x, self.position.y, new_radius)
+            asteroid.velocity = velocity_1 * 1.2
+            asteroid = Asteroid(self.position.x, self.position.y, new_radius)
+            asteroid.velocity = velocity_2 * 1.2
+            
